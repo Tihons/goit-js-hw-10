88b396fd1a1381637b15e2716e465d11765ca4dc
Tihons <tihonshramenko@gmail.com>
2023-12-17 17:35:34 +0200

    save

---
+++ src/cat-api.js
@@ -0,0 +1,40 @@
+import { onCheck } from './markup';
+import { allParameters, allReferences } from './index';
+
+export function fetchBreeds(api_key) {
+  const queryParameter = new URLSearchParams({
+    api_key,
+  });
+
+  return fetch(
+    `${allParameters.BASE_URL}${allParameters.BREEDS_END_POINT}?${queryParameter}`
+  ).then(result => {
+    if (!result) {
+      throw new Error(result.statusText);
+    }
+
+    return result.json();
+  });
+}
+
+export function fetchBreedsCurrentId(api_key, catId) {
+  const queryParameter = new URLSearchParams({
+    breed_ids: catId,
+    api_key,
+  });
+  onCheck(
+    allReferences.loaderElement,
+    allReferences.errorElement,
+    allReferences.selectContainer,
+    allReferences.catContainer
+  );
+  return fetch(
+    `${allParameters.BASE_URL}${allParameters.SEARCH_END_POINT}?${queryParameter}`
+  ).then(result => {
+    if (!result) {
+      throw new Error(result.statusText);
+    }
+
+    return result.json();
+  });
+}
---
+++ src/markup.js
@@ -0,0 +1,68 @@
+export function createMarkupSelect(arr) {
+    return arr
+      .map(
+        ({ id, name }) => `
+      <option value="${id}">${name}</option>
+    `
+      )
+      .join('');
+  }
+
+  export function createMarkupCatInfo(arr) {
+    const imgLink = arr[0].url;
+    const array = arr[0].breeds;
+  
+    return array
+      .map(
+        ({ name, description, temperament }) => `
+      <div class="div-card-cat">
+        <img  class="img-card-cat" src="${imgLink}" alt="${name}" width="300">
+        <div class="card-info-wrapper">
+          <h2 class="title-card-info">${name}</h2>
+          <p class="description-card-info">${description}<p>
+          <p class="text-card-info">
+            <span class="span-card-info">Temperament:</span>
+        ${temperament}
+          </p>
+        </div>
+      </div>
+    `
+      )
+      .join('');
+  }
+  
+  export function onStart(loading, error, divSelect, divContainer) {
+    divSelect.style.display = 'none';
+    divContainer.style.display = 'none';
+    error.style.display = 'none';
+  }
+  export function onStartOk(loading, error, divSelect, divContainer) {
+    divSelect.style.display = 'block';
+    divContainer.style.display = 'none';
+    error.style.display = 'none';
+    loading.style.display = 'none';
+  }
+  export function onStartNotOk(loading, error, divSelect, divContainer) {
+    divSelect.style.display = 'none';
+    divContainer.style.display = 'none';
+    error.style.display = 'none';
+    loading.style.display = 'none';
+  }
+  export function onCheck(loading, error, divSelect, divContainer) {
+    divSelect.style.display = 'block';
+    divContainer.style.display = 'none';
+    error.style.display = 'none';
+    loading.style.display = 'block';
+  }
+  export function onStatusOk(loading, error, divSelect, divContainer) {
+    divSelect.style.display = 'block';
+    divContainer.style.display = 'block';
+    error.style.display = 'none';
+    loading.style.display = 'none';
+  }
+  export function onStatusNotOk(loading, error, divSelect, divContainer) {
+    divSelect.style.display = 'block';
+    divContainer.style.display = 'none';
+    error.style.display = 'none';
+    loading.style.display = 'none';
+  }
